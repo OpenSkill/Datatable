55a4f2774f6e802d6c4170861713d3c440f4af3c
Nils Plaschke <n.plaschke@dsfishlabs.com>
2015-10-28 22:38:27 +0100

    set correct version for travis

--- src/Chumper/Datatable/Datatable.php
+++ src/Chumper/Datatable/Datatable.php
@@ -4,19 +4,48 @@ namespace Chumper\Datatable;
 
 use Chumper\Datatable\Providers\DTProvider;
 use Chumper\Datatable\Composers\DTDataComposer;
-use Chumper\Datatable\Composers\DTViewComposer;
+use Illuminate\Http\Request;
 
 class Datatable
 {
+    /**
+     * @var Request
+     */
+    private $request;
+
+    /**
+     * Datatable constructor. Will be resolved by laravel and will inject the needed dependencies
+     * @param Request $request The request object
+     */
+    public function __construct(Request $request)
+    {
+        $this->request = $request;
+    }
 
     /**
      * Will create a new DTDataComposer with the given provider as implementation.
      *
-     * @param DTProvider $provider The providder for the underlying data.
+     * @param DTProvider $provider The provider for the underlying data.
      * @return DTDataComposer
      */
     public function make(DTProvider $provider)
     {
         return new DTDataComposer($provider);
+    }
+
+    /**
+     * Will determine if the Datatable should handle the current request.
+     * This is normally used when just one route is active for the view and the json data.
+     *
+     * The method will check if the current request is an ajax request and if the "sEcho" or "draw" parameter is set,
+     * depending on the version of the databtable javascript.
+     *
+     * @return boolean true, if the plugin should handle this request, false otherwise
+     */
+    public function shouldHandle() {
+        if($this->request->ajax()) {
+            return true;
+        }
+        return false;
     }
 }
