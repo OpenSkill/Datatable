a8d16315041f903c002533b00e542666982bc0c6
n.plaschke <n.plaschke@dsfishlabs.com>
2015-12-12 23:56:39 +0100

    added custom order function for the CollectionProvider

--- tests/OpenSkill/Datatable/Providers/CollectionProviderTest.php
+++ tests/OpenSkill/Datatable/Providers/CollectionProviderTest.php
@@ -4,6 +4,7 @@ namespace packages\OpenSkill\Datatable\tests\OpenSkill\Datatable\Providers;
 
 use Illuminate\Support\Collection;
 use OpenSkill\Datatable\Columns\ColumnConfigurationBuilder;
+use OpenSkill\Datatable\Columns\ColumnOrder;
 use OpenSkill\Datatable\Columns\Searchable\Searchable;
 use OpenSkill\Datatable\Providers\CollectionProvider;
 use OpenSkill\Datatable\Queries\QueryConfigurationBuilder;
@@ -49,7 +50,7 @@ class CollectionProviderTest extends \PHPUnit_Framework_TestCase
         $this->assertSame(2, $data->data()->count());
 
         $first = $data->data()->first();
-        $this->assertSame(['name' => 'foo'], $first);
+        $this->assertSame(['name' => 'foo2'], $first);
     }
 
     /**
@@ -258,5 +259,63 @@ class CollectionProviderTest extends \PHPUnit_Framework_TestCase
         $data = $provider->process();
 
         $this->assertSame(1, $data->data()->count());
+    }
+
+    public function testCustomOrder()
+    {
+        $data = [
+            ['id' => 1, 'name' => 'foo'],
+            ['id' => 2, 'name' => 'bar'],
+        ];
+
+        $queryConfiguration = QueryConfigurationBuilder::create()
+            ->start(0)
+            ->length(2)
+            ->drawCall(1)
+            ->columnOrder('id', 'desc')
+            ->build();
+
+        $columnConfiguration = ColumnConfigurationBuilder::create()
+            ->name('id')
+            ->build();
+
+        $provider = new CollectionProvider(new Collection($data));
+
+        $provider->prepareForProcessing($queryConfiguration, [$columnConfiguration]);
+        $data = $provider->process();
+
+        $this->assertSame(2, $data->data()->count());
+        $first = $data->data()->first();
+
+        $this->assertSame(2, $first['id']);
+    }
+
+    public function testDefaultOrder()
+    {
+        $data = [
+            ['id' => 1, 'name' => 'foo'],
+            ['id' => 2, 'name' => 'bar'],
+        ];
+
+        $queryConfiguration = QueryConfigurationBuilder::create()
+            ->start(0)
+            ->length(2)
+            ->drawCall(1)
+            ->columnOrder('id', 'desc')
+            ->build();
+
+        $columnConfiguration = ColumnConfigurationBuilder::create()
+            ->name('id')
+            ->build();
+
+        $provider = new CollectionProvider(new Collection($data));
+
+        $provider->prepareForProcessing($queryConfiguration, [$columnConfiguration]);
+        $data = $provider->process();
+
+        $this->assertSame(2, $data->data()->count());
+        $first = $data->data()->first();
+
+        $this->assertSame(2, $first['id']);
     }
 }
